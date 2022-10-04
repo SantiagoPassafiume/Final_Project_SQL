a38e39b456eac50f5ba23b38cdb72be275c81f69
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-03 23:21:13 -0300

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -17,3 +17,21 @@ BEGIN
     
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION get_average_transaction_in_year (year_of_transaction INT)
+RETURNS DECIMAL(10,2)
+READS SQL DATA
+BEGIN
+
+	DECLARE average_transaction DECIMAL(10,2);
+
+	SELECT AVG(amount)
+	INTO average_transaction
+	FROM `transaction`
+	WHERE YEAR(`date`) = year_of_transaction;
+
+	RETURN average_transaction;
+    
+END$$
+DELIMITER ;
