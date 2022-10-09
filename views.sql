3692f9e8ad592f3a01c1b438ca602e82ae0b766c
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-08 21:58:38 -0300

    Change table names to plural

--- views.sql
+++ views.sql
@@ -1,17 +1,17 @@
 CREATE OR REPLACE VIEW all_banks AS
-	(SELECT id_bank, bank_name FROM bank);
+	(SELECT id_bank, bank_name FROM banks);
   
 CREATE OR REPLACE VIEW all_branches AS
-	(SELECT id_branch, branch_name, id_bank FROM branch);
+	(SELECT id_branch, branch_name, id_bank FROM branches);
   
 CREATE OR REPLACE VIEW all_clients AS
-	(SELECT id_account, dni, client_name FROM `client`);
+	(SELECT id_account, dni, client_name FROM clients);
   
 CREATE OR REPLACE VIEW all_accounts AS
-	(SELECT id_account, dni, id_branch, account_name FROM `account`);
+	(SELECT id_account, dni, id_branch, account_name FROM accounts);
   
 CREATE OR REPLACE VIEW all_transactions AS
-	(SELECT id_transaction, id_account, dni, amount, `date` FROM `transaction`);
+	(SELECT id_transaction, id_account, dni, amount, `date` FROM transactions);
   
 CREATE OR REPLACE VIEW highest_transaction AS
-	(SELECT MAX(amount) AS "Highest transaction" FROM `transaction`);
+	(SELECT MAX(amount) AS "Highest Transaction" FROM transactions);
