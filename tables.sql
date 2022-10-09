cea6831c7249109d26451826b3650f689a0b8ddd
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-08 21:54:16 -0300

    Change table names to plural

--- tables.sql
+++ tables.sql
@@ -1,32 +1,32 @@
 CREATE DATABASE bank_passafiume;
 USE bank_passafiume;
 
-CREATE TABLE bank (
+CREATE TABLE banks (
 	id_bank INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	bank_name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE branch (
+CREATE TABLE branches (
 	id_branch INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	branch_name VARCHAR(60) NOT NULL,
 	id_bank INT NOT NULL,
 	FOREIGN KEY (id_bank) REFERENCES bank(id_bank)
 );
 
-CREATE TABLE `account` (
+CREATE TABLE accounts (
 	id_account INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	dni INT NOT NULL,
 	id_branch INT NOT NULL,
 	account_name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE `client` (
+CREATE TABLE clients (
 	dni INT NOT NULL PRIMARY KEY,
 	id_account INT NOT NULL,
 	client_name VARCHAR(60) NOT NULL
 );
 
-CREATE TABLE `transaction` (
+CREATE TABLE transactions (
 	id_transaction INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_account INT NOT NULL,
 	dni INT NOT NULL,
@@ -34,10 +34,10 @@ CREATE TABLE `transaction` (
 	`date` DATE NOT NULL
 );
 
-ALTER TABLE `account`
-ADD FOREIGN KEY (dni) REFERENCES `client`(dni), 
-ADD FOREIGN KEY (id_branch) REFERENCES branch(id_branch);
+ALTER TABLE accounts
+ADD FOREIGN KEY (dni) REFERENCES clients(dni), 
+ADD FOREIGN KEY (id_branch) REFERENCES branches(id_branch);
 
-ALTER TABLE `client`
-ADD FOREIGN KEY (id_account) REFERENCES `account`(id_account);
+ALTER TABLE clients
+ADD FOREIGN KEY (id_account) REFERENCES accounts(id_account);
 
