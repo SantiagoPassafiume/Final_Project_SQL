34fc2d458af2aed9ef12aaac8ed373d704ff8d54
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-17 23:33:09 -0300

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -1,5 +1,21 @@
+-- Crear un usuario con el nombre "santiago" y la password "password" en localhost.
 CREATE USER santiago@localhost IDENTIFIED BY "password";
+
+/*
+Asignar los siguientes permisos para el usuario "santiago":
+1. SELECT (Lectura)
+2. INSERT (Inserción)
+3. UPDATE (Modificación)
+*/
 GRANT SELECT, INSERT, UPDATE ON *.* TO santiago@localhost;
 
+
+
+-- Crear un usuario con el nombre "debora" y la password "password" en localhost.
 CREATE USER debora@localhost IDENTIFIED BY "password";
+
+/*
+Asignar los siguientes permisos para el usuario "debora":
+1. SELECT (Lectura)
+*/
 GRANT SELECT ON *.* TO debora@localhost;
