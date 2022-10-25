e83861cb5bb52a928de5d048241b6a65afa7ac45
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-24 23:40:51 -0300

    Update sentences_tcl.sql

--- sentences_tcl.sql
+++ sentences_tcl.sql
@@ -13,11 +13,11 @@ INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(4,3,324234
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(5,2,31234334,34634,'2022-11-21');
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(6,1,30232242,21309,'2022-11-22');
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(7,3,32423434,71231,'2022-11-23');
-SAVEPOINT inserts_1;
+SAVEPOINT transactions_1;
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(8,2,31234334,21313,'2012-11-24');
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(9,1,30232242,53143,'2022-11-25');
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(10,3,32423434,63245,'2012-11-26');
 INSERT INTO transactions (id, id_account, dni, amount, `date`) VALUES(11,2,31234334,12313,'2022-11-27');
-SAVEPOINT inserts_2;
+SAVEPOINT transactions_2;
 
--- RELEASE SAVEPOINT inserts_1;
+-- RELEASE SAVEPOINT transactions_1;
