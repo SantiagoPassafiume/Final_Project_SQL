6cd70756100e321425bc5d7ea3146e3a97f204e6
PuppeteerZ <71221910+SantiagoPassafiume@users.noreply.github.com>
2022-10-26 23:07:16 -0300

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,10 +1,10 @@
 -- Mostrar todos los bancos.
 CREATE OR REPLACE VIEW all_banks AS
-	(SELECT id_bank, bank_name FROM banks);
+	(SELECT id, bank_name FROM banks);
 
 -- Mostrar todas las sucursales de bancos.
 CREATE OR REPLACE VIEW all_branches AS
-	(SELECT id_branch, branch_name, id_bank FROM branches);
+	(SELECT id, branch_name, id_bank FROM branches);
 
 -- Mostrar todos los clientes.
 CREATE OR REPLACE VIEW all_clients AS
@@ -12,11 +12,11 @@ CREATE OR REPLACE VIEW all_clients AS
  
 -- Mostrar todas las cuentas de clientes.
 CREATE OR REPLACE VIEW all_accounts AS
-	(SELECT id_account, dni, id_branch, account_name FROM accounts);
+	(SELECT id, dni, id_branch, account_name FROM accounts);
   
 -- Mostrar todas las transacciones.
 CREATE OR REPLACE VIEW all_transactions AS
-	(SELECT id_transaction, id_account, dni, amount, `date` FROM transactions);
+	(SELECT id, id_account, dni, amount, `date` FROM transactions);
 
 -- Mostrar la mayor transacción realizada.
 CREATE OR REPLACE VIEW highest_transaction AS
